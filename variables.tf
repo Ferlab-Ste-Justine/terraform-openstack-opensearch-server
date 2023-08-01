16b26c03f3830a0e38a82b5d4012ccda60b8e93f
Eric Vallee <eric_vallee@webificservices.com>
2023-08-01 18:39:11 -0400

    feat: INFRA-403 Initial port of libvirt opensearch terraform module for openstack

---
+++ variables.tf
@@ -0,0 +1,215 @@
+variable "name" {
+  description = "Name of the vm"
+  type = string
+}
+
+variable "network_port" {
+  description = "Network port to assign to the node. Should be of type openstack_networking_port_v2"
+  type        = any
+}
+
+variable "server_group" {
+  description = "Server group to assign to the node. Should be of type openstack_compute_servergroup_v2"
+  type        = any
+}
+
+variable "image_source" {
+  description = "Source of the vm's image"
+  type = object({
+    image_id = string
+    volume_id = string
+  })
+}
+
+variable "flavor_id" {
+  description = "ID of the VM flavor"
+  type = string
+}
+
+variable "keypair_name" {
+  description = "Name of the keypair that will be used by admins to ssh to the node"
+  type = string
+}
+
+variable "ssh_host_key_rsa" {
+  type = object({
+    public = string
+    private = string
+  })
+  default = {
+    public = ""
+    private = ""
+  }
+}
+
+variable "ssh_host_key_ecdsa" {
+  type = object({
+    public = string
+    private = string
+  })
+  default = {
+    public = ""
+    private = ""
+  }
+}
+
+variable "chrony" {
+  description = "Chrony configuration for ntp. If enabled, chrony is installed and configured, else the default image ntp settings are kept"
+  type        = object({
+    enabled = bool,
+    //https://chrony.tuxfamily.org/doc/4.2/chrony.conf.html#server
+    servers = list(object({
+      url = string,
+      options = list(string)
+    })),
+    //https://chrony.tuxfamily.org/doc/4.2/chrony.conf.html#pool
+    pools = list(object({
+      url = string,
+      options = list(string)
+    })),
+    //https://chrony.tuxfamily.org/doc/4.2/chrony.conf.html#makestep
+    makestep = object({
+      threshold = number,
+      limit = number
+    })
+  })
+  default = {
+    enabled = false
+    servers = []
+    pools = []
+    makestep = {
+      threshold = 0,
+      limit = 0
+    }
+  }
+}
+
+variable "install_dependencies" {
+  description = "Whether to install all dependencies in cloud-init"
+  type = bool
+  default = true
+}
+
+variable "fluentbit" {
+  description = "Fluent-bit configuration"
+  type = object({
+    enabled = bool
+    opensearch_tag = string
+    node_exporter_tag = string
+    metrics = object({
+      enabled = bool
+      port    = number
+    })
+    forward = object({
+      domain = string
+      port = number
+      hostname = string
+      shared_key = string
+      ca_cert = string
+    })
+  })
+  default = {
+    enabled = false
+    opensearch_tag = ""
+    node_exporter_tag = ""
+    metrics = {
+      enabled = false
+      port = 0
+    }
+    forward = {
+      domain = ""
+      port = 0
+      hostname = ""
+      shared_key = ""
+      ca_cert = ""
+    }
+  }
+}
+
+variable "fluentbit_dynamic_config" {
+  description = "Parameters for fluent-bit dynamic config if it is enabled"
+  type = object({
+    enabled = bool
+    source  = string
+    etcd    = object({
+      key_prefix     = string
+      endpoints      = list(string)
+      ca_certificate = string
+      client         = object({
+        certificate = string
+        key         = string
+        username    = string
+        password    = string
+      })
+    })
+    git     = object({
+      repo             = string
+      ref              = string
+      path             = string
+      trusted_gpg_keys = list(string)
+      auth             = object({
+        client_ssh_key         = string
+        server_ssh_fingerprint = string
+      })
+    })
+  })
+  default = {
+    enabled = false
+    source = "etcd"
+    etcd = {
+      key_prefix     = ""
+      endpoints      = []
+      ca_certificate = ""
+      client         = {
+        certificate = ""
+        key         = ""
+        username    = ""
+        password    = ""
+      }
+    }
+    git  = {
+      repo             = ""
+      ref              = ""
+      path             = ""
+      trusted_gpg_keys = []
+      auth             = {
+        client_ssh_key         = ""
+        server_ssh_fingerprint = ""
+      }
+    }
+  }
+
+  validation {
+    condition     = contains(["etcd", "git"], var.fluentbit_dynamic_config.source)
+    error_message = "fluentbit_dynamic_config.source must be 'etcd' or 'git'."
+  }
+}
+
+variable "opensearch" {
+  description = "Opensearch configurations"
+  type = object({
+    cluster_name          = string
+    manager               = bool
+    seed_hosts            = list(string)
+    bootstrap_security    = bool
+    initial_cluster       = bool
+    tls                   = object({
+      ca_certificate = string
+      server         = object({
+        key         = string
+        certificate = string
+      })
+      admin_client   = object({
+        key         = string
+        certificate = string
+      })
+    })
+    auth_dn_fields        = object({
+      admin_common_name = string
+      node_common_name  = string
+      organization      = string
+    })
+    verify_domains      = bool 
+    basic_auth_enabled  = bool
+  })
+}
